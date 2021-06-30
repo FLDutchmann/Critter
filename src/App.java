7beafbb0f438681e8508510dedf27df2a0c82ace
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-06-30 14:03:39 +0100

    Added win condition

--- src/App.java
+++ src/App.java
@@ -6,16 +6,19 @@ public class App {
         System.out.println("What is the critter's name?");
         String name = scanner.nextLine();
         Critter bob = new Critter(name);
-        while (bob.isAlive()) {
+        while (bob.isAlive() && !bob.hasWon()) {
             System.out.println("What would you like " + name + " to do?");
             String lineRead = scanner.nextLine();
             if (lineRead.equalsIgnoreCase("eat")) {
                 bob.feed();
             } else if (lineRead.equalsIgnoreCase("sleep")) {
                 bob.sleep();
+            } else if (lineRead.equalsIgnoreCase("exercise")) {
+                bob.exercise();
             }
         }
-        System.out.println(name + " has died.");
+        if(!bob.isAlive()) System.out.println(name + " has died.");
+        else if (bob.hasWon()) System.out.println(name + " has won the game.");
         scanner.close();
     }
 }
--- src/Critter.java
+++ src/Critter.java
@@ -1,11 +1,14 @@
 public class Critter {
     private boolean isAlive;
+    private boolean hasWon;
     private int foodLevel;
     private int tiredness;
+    private int fitness;
     private String name;
 
     public Critter(String name) {
         isAlive = true;
+        hasWon = false;
         foodLevel = 5;
         tiredness = 0;
         this.name = name;
@@ -15,33 +18,60 @@ public class Critter {
         return isAlive;
     }
 
+    public boolean hasWon() {
+        return hasWon;
+    }
+
     private void die() {
         isAlive = false;
+    }
+
+    private void win() {
+        hasWon = true;
+    }
+
+    private void addToFoodLevel(int amount) {
+        foodLevel += amount;
+        if (foodLevel <= 0) {
+            System.out.println(name + " starves to death.");
+            die();
+        }
+        if (foodLevel > 10) {
+            System.out.println(name + " over ate.");
+            die();
+        }
+    }
+
+    private void addToTiredness(int amount) {
+        tiredness += amount;
+        if (tiredness > 5) {
+            System.out.println(name + " is sleepy from so much food.");
+            sleep();
+        }
+
     }
 
     public void sleep() {
         System.out.println(name + " sleeps.");
         tiredness = 0;
-        foodLevel -= 3;
-        if (foodLevel <= 0) {
-            System.out.println(name + " starves to death.");
-            die();
-        }
+        addToFoodLevel(-3);
     }
 
     public void feed() {
         if (isAlive) {
             System.out.println(name + " eats.");
-            foodLevel++;
-            tiredness++;
-            if (foodLevel > 10) {
-                System.out.println(name + " over ate.");
-                die();
-            }
-            else if (tiredness > 5) {
-                System.out.println(name + " is sleepy from so much food.");
-                sleep();
-            }
+            addToFoodLevel(1);
+            if(isAlive) addToTiredness(1);
+        }
+    }
+
+    public void exercise() {
+        System.out.println(name + " exercises.");
+        fitness++;
+        addToTiredness(2);
+        if(fitness >= 10 && isAlive) {
+            System.out.println(name + " is in great shape.");
+            win();
         }
     }
 }
