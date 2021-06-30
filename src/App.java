98f087b793aef2bb514029a7c39080910297c3f4
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-06-30 13:29:06 +0100

    Added customisable names

--- src/App.java
+++ src/App.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Critter bob = new Critter();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the critter's name?");
+        String name = scanner.nextLine();
+        Critter bob = new Critter(name);
         while (bob.isAlive()) {
-            System.out.println("What would you like Critter to do?");
+            System.out.println("What would you like " + name + " to do?");
             String lineRead = scanner.nextLine();
             if (lineRead.equalsIgnoreCase("eat")) {
                 bob.feed();
@@ -13,7 +15,7 @@ public class App {
                 bob.sleep();
             }
         }
-        System.out.println("Critter has died.");
+        System.out.println(name + " has died.");
         scanner.close();
     }
 }
